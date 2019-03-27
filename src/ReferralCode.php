eadc5d30317bad1010a1fcc0b5009988adeda6ff
Hui Lee <lihui870920@gmail.com>
2019-03-27 16:38:30 +0800

    优化转码器

--- src/ReferralCode.php
+++ src/ReferralCode.php
@@ -4,6 +4,13 @@ namespace HuiLee\ReferralCode;
 
 class ReferralCode
 {
+    private $sourceStrings;
+
+    public function __construct()
+    {
+        $this->sourceStrings = 'E5FCDG3HQA4B1NOPIJ2RSTUV67MWX89KLYZ';
+    }
+
     /**
      * @param $id
      *
@@ -11,13 +18,12 @@ class ReferralCode
      */
     public function encryptCode($id)
     {
-        static $source_string = 'E5FCDG3HQA4B1NOPIJ2RSTUV67MWX89KLYZ';
         $num                  = $id;
         $code                 = '';
         while ($num > 0) {
             $mod  = $num % 35;
             $num  = ($num - $mod) / 35;
-            $code = $source_string[$mod].$code;
+            $code = $this->sourceStrings[$mod].$code;
         }
         if (empty($code[3])) {
             $code = str_pad($code, 4, '0', STR_PAD_LEFT);
@@ -33,7 +39,6 @@ class ReferralCode
      */
     public function decryptCode($code)
     {
-        static $source_string = 'E5FCDG3HQA4B1NOPIJ2RSTUV67MWX89KLYZ';
         if (strrpos($code, '0') !== false) {
             $code = substr($code, strrpos($code, '0') + 1);
         }
@@ -41,7 +46,7 @@ class ReferralCode
         $code = strrev($code);
         $num  = 0;
         for ($i = 0; $i < $len; ++$i) {
-            $num += strpos($source_string, $code[$i]) * pow(35, $i);
+            $num += strpos($this->sourceStrings, $code[$i]) * pow(35, $i);
         }
 
         return $num;
